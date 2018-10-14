a53026ccfe038b2acda48c52d4cd0b42cd64b49d
ie702272 <ie702272@iteso.mx>
2018-10-14 17:31:46 -0500

    Add files via upload

--- Sol_procesos.c
+++ Sol_procesos.c
@@ -20,18 +20,21 @@
 
 typedef struct _QUEUE {
 	int elements[MAXQUEUE];
-        int n_elements;
+	int head;
+	int tail;
+  int n_elements;
 } QUEUE;
 
-typedef struct semaphore_t {
-  unsigned int counter;
-  unsigned int blocked;
+typedef struct semaphore_t
+{
+  int counter;
+  //unsigned int blocked;
   QUEUE queue;
 } SEMAPHORE;
 
-char *pais[3]={"Peru","Bolvia","Colombia"};
+char *pais[3]={"Peru","Bolivia","Colombia"};
 int *g;
-int shmid;
+int *f;
 
 SEMAPHORE *sem;
 
@@ -42,21 +45,19 @@ void initqueue(QUEUE* queue)
 
 void push(QUEUE* queue, int val)
 {
-  if(queue->n_elements<MAXQUEUE){
-	queue->n_elements++;
-	queue->elements[queue->n_elements]=val;
-  }
+  queue->elements[queue->tail]=val;
+  queue->tail++;
+  queue->tail = queue->tail%MAXQUEUE;
+  queue->n_elements++;
 }
-
 int pop(QUEUE* queue)
 {
-  int Qvalue=0;
-  Qvalue = queue->elements[0];
-  for(int i=0;i<(MAXQUEUE-2);i++)
-	queue->elements[i]=queue->elements[i+1];
+  int value;
+  value = queue->elements[queue->head];
+  queue->head++;
+  queue->head = queue->head%MAXQUEUE;
   queue->n_elements--;
-  queue->elements[MAXQUEUE-1]=0;
-  return(Qvalue);
+  return(value);
 }
 
 void initsem(SEMAPHORE* sem, int value)
@@ -67,29 +68,38 @@ void initsem(SEMAPHORE* sem, int value)
 
 void wait_sem(SEMAPHORE* sem)
 {
-  
-  if(sem->counter==0)
+  int l = 1;
+  do { atomic_xchg(l,*g); } while(l!=0); // Puede pasar si otro proceso no esta en esta función
+  if(sem->counter > 0) // Si puede continuar, pasa.
   {
+    sem->counter--;
+  }
+  else // Si esta ocupado se bloquea
+  {
+    sem->counter--;
     push(&sem->queue, getpid());
-    sem->blocked++;  
-    while(1>sem->blocked);//tenemos que bloquear este proceso pero no se como 
+    kill(getpid(), SIGSTOP);
   }
-  else
-  {
-    sem->counter --;
-  }
+  *g=0; // Otro proceso puede entrar a la sección crítica
 }
 
-void signal_sem(SEMAPHORE* sem){
-
-  if(sem->blocked==0)
-   sem->counter ++;
-  else
+void signal_sem(SEMAPHORE* sem)
+{
+  int l = 1;
+  do { atomic_xchg(l,*f); } while(l!=0); // Puede pasar si otro proceso no esta en esta función
+  if((&sem->queue.n_elements) == 0) // Si no hay procesos en espera, se suma al contador
   {
-    pop(&sem->queue);
-    sem->blocked--;
-   }
- }
+    sem->counter++;
+  }
+  else if(sem->counter <= 0) // Se desbloquea el proximo proceso en la cola
+  {
+    int p_id;
+    sem->counter++;
+    p_id = pop(&sem->queue);
+    kill(p_id, SIGCONT);
+  }
+  *f=0;
+}
 
 void proceso(SEMAPHORE* sem,int i)
 {
@@ -98,14 +108,14 @@ void proceso(SEMAPHORE* sem,int i)
   for(k=0;k<CICLOS;k++)
   {
     wait_sem(sem);
-   
+
     printf("Entra %s",pais[i]);
     fflush(stdout);
     sleep(rand()%3);
     printf("- %s Sale\n",pais[i]);
-    
+
     signal_sem(sem);
-    
+
     sleep(rand()%3);
   }
   exit(0); // Termina el proceso
@@ -114,34 +124,37 @@ void proceso(SEMAPHORE* sem,int i)
 
 int main()
 {
-  SEMAPHORE semaforo;
-  semaforo.counter = 0;
-  semaforo.blocked = 0;
-  SEMAPHORE*sem = &semaforo;
-
+  
   int pid;
   int status;
   int args[3];
   int i;
+  int shmid_g, shmid_f, shmid_sem;
 
   // Solicitar memoria compartida
-  shmid=shmget(0x1234,sizeof(g),0666|IPC_CREAT);
-  if(shmid==-1)
+  shmid_g=shmget(0x1234,sizeof(g),0666|IPC_CREAT);
+  shmid_f=shmget(0x2345,sizeof(g),0666|IPC_CREAT);
+  shmid_sem=shmget(0x3456,sizeof(g),0666|IPC_CREAT);
+  if(shmid_sem == -1 || shmid_g == -1 || shmid_f == -1)
   {
     perror("Error en la memoria compartida\n");
     exit(1);
   }
 
   // Conectar la variable a la memoria compartida
-  g=shmat(shmid,NULL,0);
+  g = shmat(shmid_g,NULL,0);
+  f = shmat(shmid_f,NULL,0);
+  sem = shmat(shmid_sem,NULL,0);
 
-  if(g==NULL)
+  if(sem == NULL || g == NULL || f == NULL)
   {
     perror("Error en el shmat\n");
     exit(2);
   }
 
   initsem(sem,1);
+  *g = 0;
+  *f = 0;
 
   srand(getpid());
 
@@ -157,4 +170,6 @@ int main()
 
   // Eliminar la memoria compartida
   shmdt(g);
+  shmdt(f);
+  shmdt(sem);
 }
